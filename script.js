2b2f27644052a1432597780d342280919cdf15e6
William Garcia <66265797+PinkishOxygen@users.noreply.github.com>
2023-03-20 09:52:30 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const iframeContainer = document.getElementById("iframe-container");
-
-const iframe = document.createElement("iframe");
-iframe.src = "https://fastgpt.app";
-iframe.width = "100%";
-iframe.height = "100%";
-
-iframeContainer.appendChild(iframe);
