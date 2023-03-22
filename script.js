11de859bdec31deef899f0479e95d4928817ebf2
William Garcia <66265797+PinkishOxygen@users.noreply.github.com>
2023-03-22 19:13:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-function loadIframe(url) {
-  const iframeContainer = document.getElementById("iframe-container");
-  iframeContainer.innerHTML = "";
 
-  const iframe = document.createElement("iframe");
-  iframe.src = url;
-  iframe.style.width = "100%";
-  iframe.style.height = (window.innerHeight - 50) + "px";
-
-  iframeContainer.appendChild(iframe);
-}
-
-localStorage.setItem("sLink", sLink);
